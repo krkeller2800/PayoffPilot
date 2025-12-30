bb696765299a0bfe99692373d3040063fe7c06f7
Karl Keller <karl@komakode.com>
2025-12-30 16:05:04 -0500

    Initial Commit

---
+++ PayoffPilot/PayoffPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayoffPilotApp.swift
+//  PayoffPilot
+//
+//  Created by Karl Keller on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PayoffPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
